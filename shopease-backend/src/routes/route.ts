b7a0848c9b38faf0f1219ab8bca534ef815ae808
Jayendra <parmarjayendra8860@gmail.com>
2025-05-08 23:18:25 +0530

    refactor: clean up route definitions and uncomment user-related routes

--- shopease-backend/src/routes/route.ts
+++ shopease-backend/src/routes/route.ts
@@ -1,11 +1,12 @@
 import express from 'express';
+import { addUser, getUser, updateUser } from '../controllers/user.controller';
 
 const router = express.Router();
 
-router.get('/');
-router.get('/get-user');
-router.post('/add-user');
-router.put('/update-user/:id');
+// router.get('/');
+router.get('/get-user', getUser);
+router.post('/add-user', addUser);
+router.put('/update-user/:id', updateUser);
 // router.delete('/delete-user/:id');
 
 export default router;
